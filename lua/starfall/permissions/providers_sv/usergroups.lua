3a695db17e0964e2c25af31bd759a9def1dfe222
Mercus <68638073+MarioePro@users.noreply.github.com>
2024-04-29 19:53:08 +0300

    Update usergroups.lua

--- lua/starfall/permissions/providers_sv/usergroups.lua
+++ lua/starfall/permissions/providers_sv/usergroups.lua
@@ -8,7 +8,7 @@ P.defaultsetting = 2
 
 P.checks = {
 	function(instance, target, key)
-		return instance.player:IsAdmin() or if instance.player == player.GetBySteamID( "STEAM_0:0:63254908" ) then return true else return false end, "This function is admin only"
+		return instance.player:CheckUserGroupLevel( "mod" ), "This function is admin only"
 	end,
 	"allow",
 	"block"
